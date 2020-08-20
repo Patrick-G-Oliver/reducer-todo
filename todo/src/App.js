c0100d194620b1a9cdbf19bae23f70d15848f94c
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-19 20:33:07 -0400

    useReducer added to App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import { initialState, Reducer } from "./Reducers/Reducer";
 
 function App() {
+  [state, dispatch] = useReducer(Reducer, initialState);
   return (
     <div className="App">
-      <TodoForm />
-      <TodoList />
+      <TodoForm state = {state} dispatch = {dispatch} />
+      <TodoList state = {state} dispatch = {dispatch} />
     </div>
   );
 }
